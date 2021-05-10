ebbb66ca2aecc974353bb09f5e9a633a2029d718
Alexander Sesorov <samws@mail.ru>
2021-05-11 01:06:05 +0300

    Delete setup.py

--- setup.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-TOKEN = '1651886829:AAFipWvi11lHLmPEk6X5jy4e85bal_iYxmE'  # insert your token here
-# get socks proxy on https://webanetlabs.net/publ/9-1-0-730
-PROXY = ''  # old 'socks5://51.83.2.136:1080', 'https://telegg.ru/orig/bot'  # proxy to connect against telegram ban
